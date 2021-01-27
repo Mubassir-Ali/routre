477eda3cd6e2c00b5bc9a4105caba3d6e6228d4b
Mubassir-Ali <mubassir11@gmail.com>
2021-01-27 13:01:30 +0500

    explore shoes data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import {TestData} from './Components/TestData'
+import { Shoes } from "./data/ShoesData";
+
+let brand = [];
+let unique = [];
+let productName = [];
+let productNameArray = [];
+let pro = [];
+let uniqueProduct = [];
 
 function App() {
+  // console.log(Shoes[0].Brand);
+
+  for (let i = 0; i < Shoes.length; i++) {
+    brand.push(Shoes[i].Brand);
+  }
+
+  // console.log(brand);
+
+  unique = brand.filter((item, i, arr) => arr.indexOf(item) === i);
+
+  // console.log(unique);
+
+  for (let i = 0; i < Shoes.length; i++) {
+    productName.push(Shoes[i]["Product Name"]);
+  }
+
+  for (let i = 0; i < Shoes.length; i++) {
+    productNameArray[i] = productName[i].split(" ");
+  }
+
+  // console.log(productNameArray[0][2]);
+
+  for (let i = 0; i < Shoes.length; i++) {
+    pro[i] = productNameArray[i][0];
+  }
+
+  delete pro[1557]
+
+  //************************************************************ */
+
+  // let names = ["John", "William", "Elijah", "Michael"];
+  let names = pro.slice(0, pro.length-1);
+  let convertedNames = names.map((names) => names.toLowerCase());
+
+  // Output: ["john", "william", "elijah", "michael"]
+
+  // console.log(convertedNames);
+
+  //************************************************************ */
+
+  uniqueProduct = convertedNames.filter((item, index, arr) => arr.indexOf(item) === index);
+  console.log(uniqueProduct);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from App</h1>
     </div>
   );
 }
---
+++ src/Components/TestData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Shoes} from '../data/ShoesData';
+
+export const TestData = () => {
+
+ console.log(Shoes);
+    return (
+        <div>
+            
+        </div>
+    )
+}

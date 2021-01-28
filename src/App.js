02b15a600dcecc72ccf4560462d295b2a86eafbe
Mubassir-Ali <mubassir11@gmail.com>
2021-01-28 23:26:46 +0500

    card

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import "./App.css";
-import { Route, Switch, link, Link } from "react-router-dom";
-import { Home } from "./Components/Home";
-import { WShoes } from "./Components/WShoes";
-import { MShoes } from "./Components/MShoes";
-import { KShoes } from "./Components/KShoes";
-import { Error } from "./Components/Error";
-import WmenIndex from "./Components/WmenIndex";
-import Grid from "./Components/Grid";
-import {DataManipulation} from './Components/DataManipulation'
+import './App.css';
+import { Route, Switch, link, Link } from 'react-router-dom';
+import { Home } from './Components/Home';
+import { WShoes } from './Components/WShoes';
+import { MShoes } from './Components/MShoes';
+import { KShoes } from './Components/KShoes';
+import { Error } from './Components/Error';
+import WmenIndex from './Components/WmenIndex';
+import Grid from './Components/Grid';
+import { DataManipulation } from './Components/DataManipulation';
+import { TestCard } from './Components/TestCard';
 
-const logo =
-  "https://freepngimg.com/thumb/adidas/58143-stan-logo-smith-shoe-adidas-free-frame-thumb.png";
+const logo = 'https://freepngimg.com/thumb/adidas/58143-stan-logo-smith-shoe-adidas-free-frame-thumb.png';
 
 function App() {
-  return (
-    <div>
-      <div className="page-top" />
-      <div className="nav-bar">
-        <div className="logo">
-          <img src={logo}></img>
-        </div>
+	return (
+		<div>
+			<div className="page-top" />
+			<div className="nav-bar">
+				<div className="logo">
+					<img src={logo} />
+				</div>
 
-        <div className="menu">
-          <ul>
-            <a href="/men">
-              <li>Men</li>
-            </a>
-            <a href="/women">
-              <li>Women</li>
-            </a>
-            <a href="/kids">
-              <li>Kids</li>
-            </a>
-          </ul>
-        </div>
-      </div>
-{/* <DataManipulation/> */}
+				<div className="menu">
+					<ul>
+						<a href="/men">
+							<li>Men</li>
+						</a>
+						<a href="/women">
+							<li>Women</li>
+						</a>
+						<a href="/kids">
+							<li>Kids</li>
+						</a>
+					</ul>
+				</div>
+			</div>
+			{/* <DataManipulation/> */}
+			<TestCard />
 
-      <div className='container'>
+			{/* <div className='container'>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/women" component={WShoes} />
@@ -45,9 +46,9 @@ function App() {
           <Route exact path="/kids" component={KShoes} />
           <Route path="*" component={Error} />
         </Switch>
-      </div>
-    </div>
-  );
+      </div> */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/TestCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const TestCard = () => {
+    return (
+        <div className='card-container'>
+        <div className='card'>
+        <img src='https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/i1-adf7ff56-ce20-459a-b4a7-fdaae1afa959/image.jpg'></img>
+        <img src='https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/i1-adf7ff56-ce20-459a-b4a7-fdaae1afa959/image.jpg'></img>
+        <img src='https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/i1-adf7ff56-ce20-459a-b4a7-fdaae1afa959/image.jpg'></img>
+        <img src='https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/i1-adf7ff56-ce20-459a-b4a7-fdaae1afa959/image.jpg'></img>
+        <img src='https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/i1-adf7ff56-ce20-459a-b4a7-fdaae1afa959/image.jpg'></img>
+        </div>
+        
+                   
+        </div>
+    )
+}

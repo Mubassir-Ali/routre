bfd4a1d6e74b10d6581ed06f119e18fb780d2889
Mubassir-Ali <mubassir11@gmail.com>
2021-01-28 15:38:57 +0500

    add shoes contents

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-
-// import { Shoes } from "../data/ShoesData";
-
-// let firstFive = [];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,40 +18,32 @@ const useStyles = makeStyles((theme) => ({
 export default function CenteredGrid(props) {
   const classes = useStyles();
 
-//   firstFive = Shoes.slice(0, 20);
 
+console.log(props);
   return (
-    // <div className={classes.root}>
-    //   <Grid container spacing={3}>
-    //     {Object.entries(firstFive).map(
-    //       ([index, { Brand, Images, Description }]) => (
-    //         <Grid key={index} item xs={12} sm={6} lg={3}>
-              <Card className={classes.root}>
-                <CardActionArea>
-                  <CardMedia
-                    className={classes.media}
-                    image={props.images[0]}
-                    title="Shoes"
-                  />
-                </CardActionArea>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {props.brand}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-					align='justify'
-                  >
-                    {props.desc}
-                  </Typography>
-                </CardContent>
-              </Card>
-    //          </Grid>
-    //        )
-    //      )}
-    //    </Grid>
-    //  </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.images[0]}
+          title="Shoes"
+        />
+      </CardActionArea>
+      <CardContent>
+        <Typography component="p" align='left' variant="body2">
+          BRAND: {props.brand}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p" align="justify">
+          ID: {props.pid}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p" align="justify">
+        <span className='newPrice'>${Math.floor(props.salePrice/150)} </span><span className='discount'>${Math.floor(props.listingPrice/150)}</span>  
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p" align="justify">
+         Discount: {props.discount}%
+        </Typography>
+ 
+      </CardContent>
+    </Card>
   );
 }
--- src/Components/ShoesFilter.jsx
+++ src/Components/ShoesFilter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Shoes } from "../data/ShoesData";
 import GridShoes from "./Grid";
 
@@ -6,10 +6,10 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 let range = [];
 let productName = [];
@@ -18,36 +18,54 @@ let womenShoes = [];
 let wShoes = [];
 
 export const ShoesFilter = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    for (let i = 0; i < Shoes.length; i++) {
-        productName.push(Shoes[i]["Product Name"]);
-        womenShoesList[i] = productName[i].split(" ");
-        womenShoes.push(womenShoesList[i][0]);
-      }
-    
-      let indices = womenShoes
-        .map((e, i) => (e === props.gender ? i : ""))
-        .filter(String);
-    
-      for (let i = 0; i < indices.length; i++) {
-        wShoes[i] = Shoes[indices[i]];
-      }
-    
-    
-      range=wShoes.slice(props.start, props.end)
-    
-    return (
-        <div className={classes.root}>
-          <Grid container spacing={3}>
-            {Object.entries(range).map(
-              ([index, { Brand, Images, Description}]) => (
-                <Grid key={index} item xs={12} sm={6} lg={3}>
-                  <GridShoes brand={Brand} images={Images} desc={Description} />
-                </Grid>
-              )
-            )}
-          </Grid>
-        </div>
-      );
-}
+  for (let i = 0; i < Shoes.length; i++) {
+    productName.push(Shoes[i]["ProductName"]);
+    womenShoesList[i] = productName[i].split(" ");
+    womenShoes.push(womenShoesList[i][0]);
+  }
+
+  let indices = womenShoes
+    .map((e, i) => (e === props.gender ? i : ""))
+    .filter(String);
+
+  for (let i = 0; i < indices.length; i++) {
+    wShoes[i] = Shoes[indices[i]];
+  }
+
+  range = wShoes.slice(props.start, props.end);
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        {Object.entries(range).map(
+          ([
+            index,
+            {
+              Brand,
+              Images,
+              Description,
+              ProductID,
+              ListingPrice,
+              SalePrice,
+              Discount,
+            },
+          ]) => (
+            <Grid key={index} item xs={12} sm={6} lg={3}>
+              <GridShoes
+                brand={Brand}
+                images={Images}
+                desc={Description}
+                pid={ProductID}
+                listingPrice={ListingPrice}
+                salePrice={SalePrice}
+                discount={Discount}
+              />
+            </Grid>
+          )
+        )}
+      </Grid>
+    </div>
+  );
+};

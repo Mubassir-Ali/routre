905a05926007c5b40bc7d04372ea6a717162b6d4
Mubassir-Ali <mubassir11@gmail.com>
2021-01-31 22:21:12 +0500

    solver warning message

--- src/Components/ShoesFilter.jsx
+++ src/Components/ShoesFilter.jsx
@@ -31,7 +31,7 @@ export function ShoesFilter(props) {
 		function getAllIndexes(arr, val) {
 			var indexes = [],
 				i = -1;
-			while ((i = arr.indexOf(val, i + 1)) != -1) {
+			while ((i = arr.indexOf(val, i + 1)) !== -1) {
 				indexes.push(i);
 			}
 			return indexes;
